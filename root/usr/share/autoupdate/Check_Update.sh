c799f6ce665e9f2754e3ea389426abc47400427c
大灰狼 <81285256+shidahuilang@users.noreply.github.com>
2022-08-03 16:38:16 +0800

    ·

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -1,42 +1,52 @@
-#!/bin/sh
+#!/bin/bash
 # https://github.com/Hyy2001X/AutoBuild-Actions
 # AutoBuild Module by Hyy2001
 
-[[ -f "/tmp/Version_Tags" ]] && rm -rf /tmp/Version_Tags
 
-if [[ -f "/usr/bin/AutoUpdate" ]] && [[ -f "/bin/openwrt_info" ]]; then
-	AutoUpdate -w
-	if [[ $? -ne 0 ]]; then
+[[ -f /tmp/cloud_version ]] && rm -f /tmp/cloud_version
+[[ -f /tmp/Version_Tags ]] && rm -f /tmp/Version_Tags
+[[ -f /tmp/baidu.html ]] && rm -rf /tmp/baidu.html
+
+curl -o /tmp/baidu.html -s -w %{time_namelookup}: http://www.baidu.com
+if [[ -f /tmp/baidu.html ]] && [[ `grep -c "百度一下" /tmp/baidu.html` -ge '1' ]]; then
+	rm -rf /tmp/baidu.html
+else
+	echo "您可能没进行联网,请检查网络!" > /tmp/cloud_version
+	exit 1
+fi
+
+if [[ -f /bin/openwrt_info ]]; then
+	chmod +x /bin/openwrt_info
+	source /bin/openwrt_info
+	if [[ $? -ne 0 ]];then
+		echo "openwrt_info数据有误运行失败,请检查openwrt_info文件!" > /tmp/cloud_version
 		exit 1
-	fi
+        fi
+	chmod +x /bin/AutoUpdate.sh
+	bash /bin/AutoUpdate.sh	-w
+        if [[ $? -ne 0 ]];then
+		echo "AutoUpdate.sh数据有误运行失败,请检查AutoUpdate.sh文件!" > /tmp/cloud_version
+		exit 1
+        fi
 else
-	echo "您只编译了LCUI部分，没编译在线更新固件程序" > /tmp/cloud_version
+	echo "未检测到openwrt_info文件,无法运行更新程序!" > /tmp/cloud_version
 	exit 1
 fi
 
-if [[ -f "/tmp/Version_Tags" ]]; then
-	LOCAL_Firmware="$(grep 'LOCAL_Firmware=' "/tmp/Version_Tags" | cut -d "-" -f4)"
-	CLOUD_Firmware="$(grep 'CLOUD_Firmware=' "/tmp/Version_Tags" | cut -d "-" -f4)"
-	CLOUD_Firmware2="$(grep 'CLOUD_Firmware=' "/tmp/Version_Tags" | cut -d "=" -f2)"
-else
-	echo "未知原因获取不了版本信息" > /tmp/cloud_version
-	exit 1
-fi
-
+[[ ! -f /tmp/Version_Tags ]] && echo "未检测到云端版本,或您的仓库为私库,或您修改的Github地址有错误,或发布已被删除!" > /tmp/cloud_version && exit 1
+chmod +x /tmp/Version_Tags && source /tmp/Version_Tags
 if [[ -n "${CLOUD_Firmware}" ]]; then
-	if [[ "${LOCAL_Firmware}" == "${CLOUD_Firmware}" ]]; then
+	if [[ "${LOCAL_Firmware}" -eq "${CLOUD_Firmware}" ]]; then
 		Checked_Type="已是最新"
-		echo "${CLOUD_Firmware2} [${Checked_Type}]" > /tmp/cloud_version
-	elif [[ "${LOCAL_Firmware}" -lt "${CLOUD_Firmware}" ]]; then
-		Checked_Type="发现更高版本固件可更新"
-		echo "${CLOUD_Firmware2} [${Checked_Type}]" > /tmp/cloud_version
+		echo "${CLOUD_Firmware} [${Checked_Type}]" > /tmp/cloud_version
 	elif [[ "${LOCAL_Firmware}" -gt "${CLOUD_Firmware}" ]]; then
-		Checked_Type="云端最高版本固件,低于您现在所安装的版本,请到云端查看原因"
-		echo "${CLOUD_Firmware2} [${Checked_Type}]" > /tmp/cloud_version	
+		Checked_Type="发现更高版本固件可更新"
+		echo "${CLOUD_Firmware} [${Checked_Type}]" > /tmp/cloud_version
+	elif [[ "${LOCAL_Firmware}" -lt "${CLOUD_Firmware}" ]]; then
+		Checked_Type="云端最高版本固件,低于您现在所安装的版本"
+		echo "${CLOUD_Firmware} [${Checked_Type}]" > /tmp/cloud_version	
 	fi
 else
-	echo "未知原因获取不了云端固件的版本信息" > /tmp/cloud_version
-	exit 1
+	echo "没检测到云端固件，您可能把云端固件删除了，或格式不对称，比如很多虚拟机安装UEIF格式都会变成Legacy引导!" > /tmp/cloud_version
 fi
-
 exit 0
